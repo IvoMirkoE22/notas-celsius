17f3c102bb8597af138bdc4b17dba402e2c5bf82
Ivo Narvaez <ivomirkoelian20@gmail.com>
2022-12-07 19:59:41 -0300

    Guardar

--- src/main/java/com/celsius/notas/BaseDeDatosMemoria.java
+++ src/main/java/com/celsius/notas/BaseDeDatosMemoria.java
@@ -5,35 +5,37 @@ import java.util.List;
 
 public class BaseDeDatosMemoria implements IBaseDeDatos {
     private List<Nota> notas = new ArrayList<>();
-   // Buscamos una nota usando busqueda secuencial
-    private int buscar(Nota nota) {
-        //Comparamos el id de la nota pasada por parametro
-        // la lista
-        for (int i = 0; i< notas.size(); i++){//size tamaño de la lista
-            //notas.get(i) obtiene la nota en la posicion i
-            //notas.get(i).getId() obtiene el id de la nota
-            //equals compara dos strings
-            //nota.getId() obtiene el id de la nota pasada
-      if (notas.get(i).getId().equals(nota.getId())) {
 
-      } 
+    // Buscamos una nota usando busqueda secuencial
+    private int buscar(Nota nota) {
+        // Comparamos el id de la nota pasada por parametro
+        // la lista
+        for (int i = 0; i < notas.size(); i++) {// size tamaño de la lista
+            // notas.get(i) obtiene la nota en la posicion i
+            // notas.get(i).getId() obtiene el id de la nota
+            // equals compara dos strings
+            // nota.getId() obtiene el id de la nota pasada
+            if (notas.get(i).getId().equals(nota.getId())) {
+                return i;
+            }
         }
         return -1;
     }
-   
+
     @Override
     public void guardar(Nota nota) {
-        int indice =buscar(nota);
-        if (indice >= 0){ //si es mayor o igual a 0 entonces existe ya que -1 significa que no exixste
-             // La nota  ya existe
-             notas.set(indice, nota);//notas.set sirve para actualizar un elemento de la lista en la posicion indice el valor nota al cual estoy cambiando
+        int indice = buscar(nota);
+        if (indice >= 0) { // si es mayor o igual a 0 entonces existe ya que -1 significa que no exixste
+            // La nota ya existe
+            notas.set(indice, nota);// notas.set sirve para actualizar un elemento de la lista en la posicion indice
+                                    // el valor nota al cual estoy cambiando
 
         } else {
-            //la nota no existe
+            // la nota no existe
             nota.add(nota);
         }
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
@@ -41,13 +43,13 @@ public class BaseDeDatosMemoria implements IBaseDeDatos {
         int indice = buscar(nota);
         notas.remove(indice);
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public List<Nota> listar() {
         // TODO Auto-generated method stub
-        return null;
+        return notas;
     }
-    
+
 }
