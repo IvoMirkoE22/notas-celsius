2ffdce53b1fb0e9f186042deacd7b2e1869dbe62
Ivo Narvaez <ivomirkoelian20@gmail.com>
2022-12-06 23:15:53 -0300

    acercaDe y cometarios

--- src/main/java/com/celsius/notas/Nota.java
+++ src/main/java/com/celsius/notas/Nota.java
@@ -1,14 +1,17 @@
 package com.celsius.notas;
-
+//(f1)
+// Pojo = plain Old Java Object //una clase que representa un tipo con algunos atributos 
 import java.util.UUID;
 
 // POJO = Plain Old Java Object
-public class Nota {
+public class Nota {//extends object
   // Ctrl + . -> Generar Getters y Setters
   private String id;
   private String titulo;
   private String contenido;
 
+//Contructores puedo seleccionar Nota, poner control + . , generate contructor luego selecciono y presiono enter
+ //y me genera los contrucctores
   public Nota(String titulo, String contenido) {
     this.id=UUID.randomUUID().toString();
     this.titulo = titulo;
@@ -28,7 +31,7 @@ public class Nota {
     // this.titulo se refiere a la propiedad
     // titulo de la clase.
     // titulo sin this. se refiere al parámetro titulo
-    this.titulo = titulo;
+    this.titulo = titulo;//this a la clase actual a la de la clase padre
   }
 
   public String getContenido() {
@@ -39,11 +42,12 @@ public class Nota {
     this.contenido = contenido;
   }
 
-  @Override
-  public String toString() {
+  @Override//sobreescribo el metodo de la clase padre
+  public String toString() {//metodo publico que devolvera un tipo String
     // El control ListView llama automáticamente a este
     // método para obtener el texto a mostrar en la lista
-    return titulo;
+    // return .super.toString();//llama al metodo toString del padre por eso usa super que se usa para acceder a la clase padre de esta clase
+    return titulo;//en este caso quiero que muestre el titulo de la nota
   }
 
   public void add(Nota nota) {
--- src/main/java/com/celsius/notas/PrimaryController.java
+++ src/main/java/com/celsius/notas/PrimaryController.java
@@ -13,8 +13,8 @@ import javafx.scene.control.Alert;
 
 public class PrimaryController {
   @FXML
-  private ListView<Nota> listaNotas;
-
+  private ListView<Nota> listaNotas;//(f1) creamos un tipo personalizado que tenga un titulo y contenido
+ //Creo una clase llamada Clase.java por eso en <> pongo la clase Nota, mi 
   @FXML
   private TextField textoTitulo;
   
@@ -25,44 +25,59 @@ public class PrimaryController {
   private Button botonGuardar;
 
   @FXML
-  void salir(ActionEvent event) {
-    Alert confirmacion = new Alert(
+  void Salir(ActionEvent event) {
+      //Para preguntarle al usuario si esta seguro salir, usare la clase Alert de jf
+   Alert confirmacion = new Alert(
+      //primero espera el tipo de alerta, segundo parametro el contructor de Alert recibe un String es el mensaje que queremos mostrar,
+      //como parametro extra le agregare un boton 
       AlertType.CONFIRMATION,
-      "¿Está seguro de que desea salir?",
-      new ButtonType("Cancelar"),
-      new ButtonType("Salir")
-    );
-    // Modificamos el título y texto de cabecera del Alert
-    confirmacion.setTitle("Confirmación");
-    confirmacion.setHeaderText("Confirmación");
+      "¿Estas seguro que desea salir?",
+      new ButtonType("Cacelar"),
+      new ButtonType("Salir")//con estas don opciones sobreescribo los botones que tienen el ese dialogo
+   );
+   //Cambiaremos las palabras que estan en ingles de nuestra alerta
 
-    // respuesta -> {} es una función lambda, que es una
-    // función anónima. Es un concepto complejo que no vamos
-    // a ver en el curso.
-    confirmacion.showAndWait().ifPresent(respuesta -> {
-      // Si el ButtonType tiene el texto "Salir"
-      // cerramos la aplicación
-      if (respuesta.getText().equals("Salir")) {
-        // Método para salir de una aplicación de JavaFX
-        Platform.exit();
+   confirmacion.setTitle("Confirmación");//modifique el titulo ingles a español con setTitle que nos provee alert
+   confirmacion.setHeaderText("Confirmación");//encabezado en español, donde en este caso aparece un icono (? azul y blanco)
+   //confirmacion.show();//esto quiere decir que llamo al metodo show de la instancia de Alert confirmacion,metodo es una funcion que pertenece a una clase o instancia de clase
+   //respuesta -> {} es una funcion lambda, es una funcion anonima.Es un concepto complejo
 
-        // Método para terminar un proceso de Java
-        // El parámetro es el código de error. Si no hay
-        // error, va 0.
-        System.exit(0);
-      }
-    });
+   confirmacion.showAndWait().ifPresent(respuesta-> { 
+      System.out.println(respuesta.getText());
+   //Quiero hacer que haga algo cuando le doy a salir,showAndWait muestra el dialogo y espera una respuesta,usaremos 
+   //una funcion landam dentro de ifPresent() es una funcion anonima que se implementa en una sola linea sin tipo ni nada, ifpresent si aprete algun boton me dara la respuesta,
+   //la respuesta sera el boton que aprete o salir o cancelar con respuesta.getText
+   //Ahora para chequear si es salir salga de la aplicacion o para hacer algo usamos if
+
+   //Si el ButtonType tiene el texto "Salir"
+   //Cerramos la aplicacion
+   if (respuesta.getText().equals("Salir")){//equals es ==
+      
+      System.out.println("Salir del programa");
+      //Para salir efectivamente del programa es
+      Platform.exit();//Método para salir de una aplicacion de JavaFX
+      System.exit(0);//Metodo para terminar un proceso de java, el parametro es el codigo de error, si no hay error, va 0
+      //0 es el codigo de error, si se coloca cualquier cosa distinta de cero
+      //Quiere decir que se cerro con un error, ponemos cero porque cerraremos porque queremos 
+   }
+  });
   }
 
   @FXML
   void acercaDe(ActionEvent event) {
-    // Mostrar un Alert con información acerca del
-    // programa.
+    Alert informacion = new Alert(
+       AlertType.INFORMATION,
+       "Celsius Notas v1.0 Desarrolado por Ivo Narvaez. El sistema operativo Windows y su interfaz de usuario están protegidos" 
+       );
+       informacion.show();//Llamo al metodo show de la instancia de Alerta (la clase) informacion
+       //un metodo simplemente es una funcion que pertenece a una clase o instancia de clase, es una funcion basicamente
+       //Cambiaremos las palabras que estan en ingles de nuestra alerta
 
-    // Ej: Celsius Notas v1.0
-    //     Desarrollado por Mariano Córdoba
-  }
+       informacion.setTitle("Acerca de bloc de notas");//modifique el titulo ingles a español con setTitle que nos provee alert
+       informacion.setHeaderText("Windows ");//encabezado en español, donde en este caso aparece un icono (? azul y blanco)
 
+       
+    }
   @FXML
   void eliminarNota(ActionEvent event) {
 
@@ -70,8 +85,9 @@ public class PrimaryController {
 
   @FXML
   void crearNota(ActionEvent event) {
+    //(f1)Creamos una clase para representar una nota
     Nota nota = new Nota("Nota sin título", "");
-    listaNotas.getItems().add(nota);
+    listaNotas.getItems().add(nota);//Agrego  una nota a mi lista de notas, // ver a fondo en el curso
   }
 
   @FXML
